635f24bfa94c7544f28b27de6ba27d73c5d7e38f
RonakSingh2006 <ronaksingh201106@gmail.com>
2025-12-21 02:02:39 +0530

    updated input element

--- apps/frontend/components/Input.tsx
+++ apps/frontend/components/Input.tsx
@@ -1,8 +1,15 @@
+import { forwardRef } from "react";
+
 interface InputProps{
   placeholder : string,
   type : "text" | "password"
 }
 
-export default function Input({placeholder,type} : InputProps){
-  return <input type={type} placeholder={placeholder} className="bg-zinc-500 w-72 p-2 outline none rounded-md text-lg"/>
-}
+
+const Input = forwardRef<HTMLInputElement,InputProps>((props,ref)=> {
+  return <input type={props.type} placeholder={props.placeholder} className="bg-zinc-500 w-72 p-2 outline none rounded-md text-lg" ref={ref}/>;
+});
+
+Input.displayName = "Input";
+
+export default Input;
