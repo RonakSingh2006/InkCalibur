1ff2230f973146fbd16f518d0b77118be38dead3
RonakSingh2006 <ronaksingh201106@gmail.com>
2025-12-21 10:53:59 +0530

    fixed path error

--- apps/frontend/lib/checkUser.ts
+++ apps/frontend/lib/checkUser.ts
@@ -1,10 +1,11 @@
+import { BACKEND_URL } from "@repo/common/config";
 import axios from "axios";
 export default async function checkUser(){
   const token = localStorage.getItem('token');
 
   if(!token) return false;
 
-  const response = await axios.get("/me",{
+  const response = await axios.get(`${BACKEND_URL}/me`,{
     headers : {"authorization" : token}
   })
 
