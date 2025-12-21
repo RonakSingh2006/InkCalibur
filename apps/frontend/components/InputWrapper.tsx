683bc68c49c84220e236d421a629edefb993ca4c
RonakSingh2006 <ronaksingh201106@gmail.com>
2025-12-21 11:03:18 +0530

    fixed position of error in inputwarpper

--- apps/frontend/components/InputWrapper.tsx
+++ apps/frontend/components/InputWrapper.tsx
@@ -1,8 +1,11 @@
 import { ReactNode } from "react";
 
 export default function InputWrapper({children,error} : {children : ReactNode , error : string}){
-  return<div className="min-h-16 flex flex-col justify-center">
-          {children}
-        {error && <p className="text-red-400 text-sm text-center">{error}</p>}
+  return<div className="h-16 flex flex-col justify-center relative">
+        {children}
+
+        {error && <div className="absolute z-2 top-15 w-72">
+            <p className="text-red-400 text-sm text-center">{error}</p>
+          </div>}
     </div>
 }
