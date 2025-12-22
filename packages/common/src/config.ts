e27f4fc4a5610d603d15b45023e9e432d3d56c02
RonakSingh2006 <ronaksingh201106@gmail.com>
2025-12-23 01:30:43 +0530

    removed temp token

--- packages/common/src/config.ts
+++ packages/common/src/config.ts
@@ -1,4 +1,2 @@
 export const BACKEND_URL = `http://localhost:3001`
 export const WS_URL =  `ws://localhost:8080`
-
-export const temp_JWT = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJmMTJlYzMxMS1iMmQzLTRlOTItOGM3Ny1hNTMwNzZlOGNmNWIiLCJpYXQiOjE3NjYxMzM4MTN9.xVf-5eGTgvSr_n-Wv8-RbacBDUcjjY2H9T5rhz1RiYg"
