35744c43d07aab64e7153029e8509eadee3d9c0b
RonakSingh2006 <ronaksingh201106@gmail.com>
2025-12-22 23:08:39 +0530

    removed chat code

--- apps/web/app/room/[slug]/page.tsx
+++
@@ -1,8 +0,0 @@
-import ChatRoom from "../../components/ChatRoom";
-
-export default async function Room({params} : {params : Promise<{slug : string}>}){
-  const {slug} = await params;
-  return <div>
-    <ChatRoom slug={slug}></ChatRoom>
-  </div>
-}
